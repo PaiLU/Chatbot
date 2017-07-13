2698ed28a505c2e3cc39ad35339820d1c0dc7d3b
PaiLU <lupai1996@gmail.com>
2017-07-13 16:43:38 +0800

    Update addID and password

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 //Create chat bot
 var connector = new builder.ChatConnector
-({ appId:'',appPassword:''});
+({ appId:'f529a65d-4900-451c-b04e-36371442f69a',appPassword:'S4qOp6L0ifSfbbbi8O58Bvk'});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages',connector.listen());
 
