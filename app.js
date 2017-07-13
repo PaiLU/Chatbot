2805f8918a83df889f95b4fa626d3a49f02fd20a
PaiLU <lupai1996@gmail.com>
2017-07-13 17:11:02 +0800

    New Codes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Add your requirement
+/* Add your requirement
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -18,4 +18,27 @@ server.post('/api/messages',connector.listen());
 //Create bot dialogs
 bot.dialog('/',function(session){
     session.send("Hello World");
+}); */
+
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
 });
