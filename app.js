03caac6961d85a258f1111bc22f949f6c96b6cd1
PaiLU <lupai1996@gmail.com>
2017-07-13 17:53:31 +0800

    Change to testing1

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-/* Add your requirement
+//Add your requirement
 var restify = require('restify');
 var builder = require('botbuilder');
 
 //Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function()
-{
-    console.log('%s listening to %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
 
 //Create chat bot
 var connector = new builder.ChatConnector
 ({ appId:'f529a65d-4900-451c-b04e-36371442f69a',appPassword:'S4qOp6L0ifSfbbbi8O58Bvk'});
+
+// Listen for messages from users 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages',connector.listen());
 
 //Create bot dialogs
 bot.dialog('/',function(session){
     session.send("Hello World");
-}); */
+}); 
 
-var restify = require('restify');
+/*var restify = require('restify');
 var builder = require('botbuilder');
 
 // Setup Restify Server
