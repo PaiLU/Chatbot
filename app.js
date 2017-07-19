1af074a37487b2e432c53a0493c18637e30dcce8
PaiLU <lupai1996@gmail.com>
2017-07-19 11:47:46 +0800

    Conversation

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-/*//Add your requirement
-var restify = require('restify');
-var builder = require('botbuilder');
-//Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-//Create chat bot
-var connector = new builder.ChatConnector
-({ appId:'f529a65d-4900-451c-b04e-36371442f69a',appPassword:'S4qOp6L0ifSfbbbi8O58Bvk'});
-// Listen for messages from users 
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages',connector.listen());
-//Create bot dialogs
-bot.dialog('/',function(session){
-    session.send("Hello World");
-}); */
-
+//Add your requirement
 var restify = require('restify');
 var builder = require('botbuilder');
 
@@ -35,10 +17,33 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+// Receive messages from the user only
+var bot = new builder.UniversalBot(connector);
+/*// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
 });
+//Create bot dialogs
+bot.dialog('/',function(session){
+    session.send("Hello World");
+});*/
+//var bot = new builder.TextBot();
+bot.dialog('/', function (session) {
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+    }
+});
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 
 server.get('/', restify.plugins.serveStatic({
  directory: __dirname,
