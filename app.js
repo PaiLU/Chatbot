f0769c4361f775d014be796e60176989051a707c
PaiLU <lupai1996@gmail.com>
2017-07-24 18:42:48 +0800

    Flight Booking test

--- app.js
+++ app.js
@@ -21,7 +21,44 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user only
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+});
+
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+
+bot.dialog('BookFlight', [
+    function (session, args, next) {
+        session.send('Welcome to the Flight Booking Engine! We are analyzing your message: \'%s\'', session.message.text);
+
+        // try extracting entities
+        var ToLocation = builder.EntityRecognizer.findEntity(args.intent.entities, 'ToLocation');
+        if (ToLocation) {
+            // city entity detected, continue to next step
+            session.dialogData.searchType = 'ToLocation';
+            next({ response: ToLocation.entity });
+        }  else {
+            // no entities detected, ask user for a destination
+            builder.Prompts.text(session, 'Please enter your destination');
+        }
+    }, 
+
+    function (session, results) {
+        var destination = results.response;
+
+        var message = 'Looking for flights';
+        message += 'to %s...';
+        session.send(message, destination);
+    }
+    
+]).triggerAction({
+    matches: 'BookFlight'
+});
+
+
+
+//var bot = new builder.UniversalBot(connector);
 /*// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
@@ -31,7 +68,7 @@ bot.dialog('/',function(session){
     session.send("Hello World");
 });*/
 //var bot = new builder.TextBot();
-bot.dialog('/', function (session) {
+/*bot.dialog('/', function (session) {
     if (!session.userData.name) {
         session.beginDialog('/profile');
     } else {
@@ -39,7 +76,6 @@ bot.dialog('/', function (session) {
         session.userData.name = null;
     }
 });
-
 bot.dialog('/profile', [
     function (session) {
         builder.Prompts.text(session, 'Hi! What is your name?');
@@ -50,7 +86,9 @@ bot.dialog('/profile', [
     }
 ]);
 
+
 server.get('/', restify.plugins.serveStatic({
  directory: __dirname,
  default: '/index.html'
 }));
+*/
