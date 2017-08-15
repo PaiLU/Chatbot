d05f464198ab7c35306173a3fcae7c2d093a481a
PaiLU <lupai1996@gmail.com>
2017-08-15 16:49:30 +0800

    Key Correction

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 })
 var connector = new builder.ChatConnector({
-        appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-    //appId: process.env.MY_APP_ID,
-    //appPassword: process.env.MY_APP_PASSWORD
+    //appId: process.env.MICROSOFT_APP_ID,
+    //appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MY_APP_ID,
+    appPassword: process.env.MY_APP_PASSWORD
 });
 
 // Listen for messages from users and Receive messages
