23398b0369d15b98871c05d04f934ee1198d34a1
PaiLU <lupai1996@gmail.com>
2017-09-28 11:29:15 +0800

    geting restify server

--- app.js
+++ app.js
@@ -2,15 +2,16 @@
 require('dotenv-extended').load();
 var restify = require('restify');
 var builder = require('botbuilder');
+var http = require('http');
 var server = restify.createServer();
 server.listen(process.env.port || 3978, function(){
     console.log('%s listening to %s', server.name, server.url);
 })
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD,
-    // appId: process.env.MY_APP_ID,
-    // appPassword: process.env.MY_APP_PASSWORD,
+    // appId: process.env.MICROSOFT_APP_ID,
+    // appPassword: process.env.MICROSOFT_APP_PASSWORD,
+    appId: process.env.MY_APP_ID,
+    appPassword: process.env.MY_APP_PASSWORD,
 });
 server.post('api/messages',connector.listen());
 var bot = new builder.UniversalBot(connector, function(session){
@@ -35,7 +36,7 @@ bot.dialog('reqStatus', [
         var options = {
             host: localhost,
             port: 80,
-            path: '/api/leavedetail',
+            path: '/api/leave',
             method: 'GET'
         };
         http.request(options, function(res) {
