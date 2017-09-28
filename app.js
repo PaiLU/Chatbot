3f1156df818f8e0a585f0872e1003cd4dded2401
PaiLU <lupai1996@gmail.com>
2017-09-28 17:03:32 +0800

    check server

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ server.listen(process.env.port || 3978, function(){
     console.log('%s listening to %s', server.name, server.url);
 })
 var connector = new builder.ChatConnector({
-    // appId: process.env.MICROSOFT_APP_ID,
-    // appPassword: process.env.MICROSOFT_APP_PASSWORD,
-    appId: process.env.MY_APP_ID,
-    appPassword: process.env.MY_APP_PASSWORD,
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD,
+    // appId: process.env.MY_APP_ID,
+    // appPassword: process.env.MY_APP_PASSWORD,
 });
 server.post('api/messages',connector.listen());
 var bot = new builder.UniversalBot(connector, function(session){
-    session.send('Hi, I can\'t understand what you are entered. <br\>You can apply leave or ask for your leave balance <be\>Type \'help\' anytime if you need assistance');
+    session.send('%s listening to %s', JSON.stringify(server.name), JSON.stringify(server.url));
+    session.endConversation('Hi, I can\'t understand what you are entered. <br\>You can apply leave or ask for your leave balance <be\>Type \'help\' anytime if you need assistance');
 });
 var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL_LeaveBot);
 bot.recognizer(recognizer);
