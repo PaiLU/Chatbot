acb2c81e53bcf4d2674f304f3a96bee86856752d
PaiLU <lupai1996@gmail.com>
2017-10-23 18:26:54 +0800

    ok

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ require('dotenv-extended').load();
 var restify = require('restify');
 var builder = require('botbuilder');
 var http = require('http');
-var server = restify.createServer();
 var getName = require('./name');
+var server = restify.createServer();
 server.listen(process.env.port || 3978, function(){
     console.log('%s listening to %s', server.name, server.url);
 })
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD,
-    // appId: process.env.MY_APP_ID,
-    // appPassword: process.env.MY_APP_PASSWORD,
+    // appId: process.env.MICROSOFT_APP_ID,
+    // appPassword: process.env.MICROSOFT_APP_PASSWORD,
+    appId: process.env.MY_APP_ID,
+    appPassword: process.env.MY_APP_PASSWORD,
 });
 server.post('api/messages',connector.listen());
 
