c53e7297077796f9f91d54d64d0fbdd3b334cb3e
PaiLU <lupai1996@gmail.com>
2017-10-26 13:26:12 +0800

    try session.message

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ var http = require('http');
 var server = restify.createServer();
 
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD,
-    // appId: process.env.MY_APP_ID,
-    // appPassword: process.env.MY_APP_PASSWORD,
+    // appId: process.env.MICROSOFT_APP_ID,
+    // appPassword: process.env.MICROSOFT_APP_PASSWORD,
+    appId: process.env.MY_APP_ID,
+    appPassword: process.env.MY_APP_PASSWORD,
 });
 server.post('api/messages',connector.listen());
 
