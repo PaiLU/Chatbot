50b2fb0755144c362fac9ad77efb852f38b93093
PaiLU <lupai1996@gmail.com>
2018-01-25 15:18:40 +0800

    no change

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var http = require('http');
 var server = restify.createServer();
 
 var connector = new builder.ChatConnector({
-    // appId: process.env.MY_APP_ID,
-    // appPassword: process.env.MY_APP_PASSWORD,
+    // appId: process.env.MICROSOFT_APP_ID,
+    // appPassword: process.env.MICROSOFT_APP_PASSWORD,
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
     openIdMetadata: process.env.BotOpenIdMetadata 
